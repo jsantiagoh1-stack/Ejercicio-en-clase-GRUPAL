e6c55c8b4f0f60c771a7c539a352bf2fff97abdd
Antony Najarro <anajarrom@miumg.edu.gt>
2026-09-04 17:40:57 -0600

    Feat: Registrar notas

--- src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
+++ src/main/java/edu/umg/programacion2/clase07/inscripciones/dao/InscripcionDAO.java
@@ -29,7 +29,7 @@ public class InscripcionDAO {
 
     private static final String URL = "jdbc:mysql://localhost:3306/prog2_db?useSSL=false&serverTimezone=UTC";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "111.";
+    private static final String PASSWORD = "Tonyñi123.";
 
     /**
      * Inscribe a un estudiante en un curso. Retorna el id generado.
@@ -89,8 +89,18 @@ public class InscripcionDAO {
      *    EstudianteDAO.actualizarNombre en la Clase 5).
      */
     public boolean registrarNota(int estudianteId, int cursoId, double nota) throws SQLException {
-        // TODO: completar.
-        return false;
+        String sql = "UPDATE inscripciones SET nota = ? WHERE estudiante_id = ? AND curso_id = ?";
+
+        try (Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+             PreparedStatement statement = conexion.prepareStatement(sql)) {
+
+            statement.setDouble(1, nota);
+            statement.setInt(2, estudianteId);
+            statement.setInt(3, cursoId);
+
+            int filasAfectadas = statement.executeUpdate();
+            return filasAfectadas > 0;
+        }
     }
 
     /**
